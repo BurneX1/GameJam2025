c834fe42063df9e115bd94f653d72e21b4557227
Alejandro Medina <almpratto@gmail.com>
2025-01-26 05:59:54 -0500

    Modules Update

---
+++ Assets/Scripts/GameManagment/ModGenerator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModGenerator : MonoBehaviour
+{
+    public GameObject[] startModules;
+
+    public GameObject[] Bmodules;
+
+
+    public GameObject[] endModules;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class ModulePack
+{
+    public Transform spawn;
+
+
+}
